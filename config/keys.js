0e7f10f35c5c9249f5066af284361939eaf41155
StarfoxMulder <cjprestia@gmail.com>
2018-04-22 20:33:02 -0400

    initial Express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://jude:jude@ds117821.mlab.com:17821/devconnector22"
+};
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users.js");
+const profile = require("./routes/api/profile.js");
+const posts = require("./routes/api/posts.js");
+
+const app = express();
+
+//DB COnfig
+const db = require("./config/keys.js").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello"));
+
+// User Routes
+// When /api/users is in the path, the router will look for what to do with the request in the users.js (since we assigned 'users' the value of that file's location)
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));

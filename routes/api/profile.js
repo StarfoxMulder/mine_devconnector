f9c0583613efc38838030e27a8e0f1c69d540914
StarfoxMulder <cjprestia@gmail.com>
2018-04-23 22:06:04 -0400

    Added Profile Schema and initial profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const mongoose = require("mongoose");
+
+// Loading Profile and User models
+const Profile = require("../../models/profile");
+const User = require("../../models/User");
 
 router.get("/test", (req, res) => res.json({ msg: "Profile works" }));
+
+// @route   GET api/profile/
+// @desc    get current user's profile
+// @access  Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Since the Profile and User models are linked via the id assigned to that user's record, we can pass in req.user.id as the value we want to have mongoose search MongoDB for a match.  This 'user: req.user.id' will search the Profile schema for a record with that value and return the full profile object associated with it.
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;

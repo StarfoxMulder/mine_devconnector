e1b4d53c74d276b1f0c1b0bb404521c8919bcc5b
StarfoxMulder <cjprestia@gmail.com>
2018-04-27 20:32:23 -0400

    front end routing, login validation, and introduction of redux/reducers

---
+++ client/src/store.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+// becasue no file name is specified within /reducers, index.js is assigned by default.  './reducers' === './reducers/index.js'
+
+const initialState = {};
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
+
+/****
+ * createStore() accepts 3 arguments
+ * the first is the reducer, originally symbolized by '() => []
+ * the second is initial state, originally symbolized by an empty object '{}'
+ * the third is for middleware
+ */
